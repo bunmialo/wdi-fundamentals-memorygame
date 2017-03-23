6000af6d9c1fc609deb654b65c67654b7c0d224d
Bunmi Alo <bunmialo@yahoo.com>
2017-03-23 09:31:09 -0400

    added cards comparison

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,13 @@ var cardOne = "queens";
 var cardTwo = "queens";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+    alert ("You found a match!");
+
+} else if (cardOne === cardTwo) {
+    alert ("You found a match!");
+
+} else {
+    alert("Sorry, try again.");
+}
